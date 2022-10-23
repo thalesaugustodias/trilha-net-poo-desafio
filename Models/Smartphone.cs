b73b29d5e8875bda1aa365236f604f002223f57f
thales.cooperx <thales.cooperx@gmail.com>
2022-10-22 23:41:20 -0300

    Projeto-POO-Bootcamp-Pottencial-DotNet-Developer

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -6,7 +6,6 @@ namespace DesafioPOO.Models
         private string _modelo;
         private string _imei;
         private int _memoria;
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
@@ -14,7 +13,6 @@ namespace DesafioPOO.Models
             _modelo = modelo;
             _imei = imei;
             _memoria = memoria;
-            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
